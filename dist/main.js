83b8a325b2c8e5583cf99d8541ae5292aefd45d5
Yann Klein <yann.klein@me.com>
2021-05-12 17:54:38 +0900

    buil lc

--- dist/main.js
+++ dist/main.js
@@ -95,48 +95,41 @@
 
 const authorization = "Bearer sk_f9deb0090ac7688f94eb11ce09fd6a6e";
 
-// 1. Select button, input and the four field
-const input = document.querySelector("#clearbitEmail");
-const button = document.querySelector("#clearbitSubmit");
-
-const name = document.querySelector("#userName");
+// 1. Select all the elements eg input, button, username, email, bio, location
+const input = document.querySelector("#clearbitForm input");
+const form = document.querySelector("#clearbitForm");
+const username = document.querySelector("#userName");
 const email = document.querySelector("#userEmail");
-const bio = document.querySelector("#userBio");
 const location = document.querySelector("#userLocation");
+const bio = document.querySelector("#userBio");
 
-const stalkPerson = (event) => {
-  // 3. preventDefault()
-  event.preventDefault();
-  // 4. Fetch info from ClearAPI
-  // (GET? POST? => check the documentation)
-  fetchClearbit(input.value);
+const display = (data) => {
+  username.innerHTML = data.name.fullName;
+  email.innerHTML = data.email;
+  location.innerHTML = data.location;
+  bio.innerHTML = data.bio;
+  
 }
 
-const fetchClearbit = (emailToStalk) => {
-  const url = `https://person.clearbit.com/v2/combined/find?email=${emailToStalk}`;
+// 2. Listen to submit button
+form.addEventListener("submit", (event) => {
+  // 2.5 preventDefault()
+  event.preventDefault();
+  const url = `https://person.clearbit.com/v2/combined/find?email=${input.value}`;
+  // 3. Fetch API
   fetch(url, {
-    headers: { "Authorization": authorization }
+    headers: {
+      'Authorization': authorization
+    }
   })
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data.person);
-      // 5. Display the info in the 4 fields
-      displayClearbitInfo(data.person);
-
-    });
-};
-
-const displayClearbitInfo = (personData) => {
-  name.innerText = personData.name.fullName;
-  email.innerText = personData.email;
-  bio.innerText = personData.bio;
-  location.innerText = personData.location;
-};
-
-// 2. Listen to a click on submit
-button.addEventListener("click", stalkPerson);
-
-
+  .then(response => response.json())
+  .then((data) => {
+    // 4. Display
+    const personData = data.person;
+    console.log(personData);
+    display(personData);
+    })
+});
 
 /***/ })
 
